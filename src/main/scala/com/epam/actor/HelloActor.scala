1005becbafa570486273177095f0721d5a607f28
Taras_Fedorov <CkfdfDtktce06~>
2018-04-23 17:56:30 +0300

    Add actor

---
+++ src/main/scala/com/epam/actor/HelloActor.scala
@@ -0,0 +1,22 @@
+package com.epam.actor
+
+import akka.actor.Actor
+import akka.actor.ActorSystem
+import akka.actor.Props
+
+class HelloActor extends Actor {
+  private[this] var encapsulatedDate = "Shared nothing"
+
+  def receive = {
+    case "hello" => println("hello back at you")
+    case _ => println("huh?" + encapsulatedDate)
+  }
+}
+
+object Main extends App {
+  val system = ActorSystem("HelloSystem")
+  // default Actor constructor
+  val helloActor = system.actorOf(Props[HelloActor], name = "helloactor")
+  helloActor ! "hello"
+  helloActor ! "buenos dias"
+}
---
+++ src/main/scala/com/epam/actor/PingPongTest.scala
@@ -0,0 +1,55 @@
+package com.epam.actor
+
+import akka.actor._
+
+
+case class PingMessage(num: Int)
+
+case object PongMessage
+
+case object StartMessage
+
+case class StopMessage(reason: String)
+
+
+class Ping(pong: ActorRef) extends Actor {
+  private[this] var internalState = 0
+
+  def receive = {
+    case StartMessage =>
+      pong ! PingMessage(internalState)
+
+    case PongMessage =>
+      if (internalState > 99)
+        sender ! StopMessage("Stopped by pong")
+      else {
+        internalState += 1
+        println("ping - " + internalState)
+        sender ! PingMessage(internalState)
+      }
+  }
+
+}
+
+class Pong extends Actor {
+  def receive = {
+    case PingMessage(num) =>
+      println("  pong - " + num)
+      sender ! PongMessage
+    case StopMessage(reason: String) =>
+      println("pong stopped" + reason)
+      context.stop(self)
+  }
+}
+
+object PingPongTest extends App {
+  val system = ActorSystem("PingPongSystem")
+  val pong = system.actorOf(Props[Pong], name = "pong")
+  val ping = system.actorOf(Props(new Ping(pong)), name = "ping")
+  // start them going
+  ping ! StartMessage
+
+  Thread.sleep(155)
+  pong ! StopMessage("Stopped by main App")
+
+}
